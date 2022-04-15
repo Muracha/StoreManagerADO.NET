759b8f08f42c1d0c1a64761ea3f633c95a9de289
M M9 <murachamuracha@gmail.com>
2022-04-15 22:48:48 +0400

    Adding variables to Product

--- G02_StoreManager/StoreManager.Models/Product.cs
+++ G02_StoreManager/StoreManager.Models/Product.cs
@@ -11,5 +11,7 @@ namespace StoreManager.Models
         public int ID { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public string CreateDate { get; set; }
+        public string Description { get; set; }
     }
 }
