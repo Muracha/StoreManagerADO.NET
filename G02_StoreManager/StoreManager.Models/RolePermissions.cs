ba3060a5241d4790792be51b757f5bbc3ee1a25f
Sandro Dolidze <dolidzesandro8@gmail.com>
2022-04-15 23:55:34 +0400

    Adding RolePermissions class

---
+++ G02_StoreManager/StoreManager.Models/RolePermissions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Models
+{
+    public class RolePermissions
+    {
+        public int RoleID { get; set; }
+        public int PermissionID { get; set; }
+
+    }
+}
