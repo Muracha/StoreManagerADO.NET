dcb640328e08db11553eec37551631351e6a5ded
M M9 <murachamuracha@gmail.com>
2022-04-15 22:58:10 +0400

    Adding Contact class and it's variables

---
+++ G02_StoreManager/StoreManager.Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Models
+{
+    class Contact
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Data { get; set; }
+        public string CreateDate { get; set; }
+    }
+}
