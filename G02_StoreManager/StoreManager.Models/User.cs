d5294b348b3efe01c19e5167ad250f343aa9676d
M M9 <murachamuracha@gmail.com>
2022-04-15 22:51:47 +0400

    Adding variables to User

--- G02_StoreManager/StoreManager.Models/User.cs
+++ G02_StoreManager/StoreManager.Models/User.cs
@@ -8,6 +8,9 @@ namespace StoreManager.Models
 {
     public class User
     {
-
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string CreateDate { get; set; }
     }
 }
