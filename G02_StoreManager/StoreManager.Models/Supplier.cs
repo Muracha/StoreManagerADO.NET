e9dd0eacef093b10fe4c0cefb59e48184d03218e
M M9 <murachamuracha@gmail.com>
2022-04-15 22:47:24 +0400

    Adding variables to Supplier

--- G02_StoreManager/StoreManager.Models/Supplier.cs
+++ G02_StoreManager/StoreManager.Models/Supplier.cs
@@ -9,6 +9,11 @@ namespace StoreManager.Models
 	public class Supplier
 	{
 		public int ID { get; set; }
-
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public string Phone { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
+		public int Fax { get; set; }
 	}
 }
