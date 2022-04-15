5333c5f0dce752979af17ec8795cd597178bd41b
Sandro Dolidze <dolidzesandro8@gmail.com>
2022-04-15 23:51:35 +0400

    Adding TransactionDetails class

---
+++ G02_StoreManager/StoreManager.Models/TransactionDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Models
+{
+    public class TransactionDetails
+    {
+        public int TransactionID { get; set; }
+        public int ProductID { get; set; }
+        public decimal SellPrice { get; set; }
+        public decimal Quantity { get; set; }
+    }
+}
