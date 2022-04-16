2ff5fcdcb1c1deaed2f1aa46e71c38f8c6ffb0c9
ana liluashvili <liluashvilia48@gmail.com>
2022-04-16 14:41:58 +0400

    change class Role

--- G02_StoreManager/StoreManager.Models/Role.cs
+++ G02_StoreManager/StoreManager.Models/Role.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StoreManager.Models
 {
-    class Role
+    public class Role
     {
         public int ID { get; set; }
         public string Name { get; set; }
