4fce1b841766182daf8930f283fc363d540ca0a5
David Dvali <david.dvali@gmail.com>
2022-04-16 20:01:10 +0400

    Add methods to user repository

--- G02_StoreManager/StoreManager.Repositories/UserRepository.cs
+++ G02_StoreManager/StoreManager.Repositories/UserRepository.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using StoreManager.Models;
+using DataHelper;
 
 namespace StoreManager.Repositories
 {
-    public class UserRepository
-    {
-    }
+	public class UserRepository
+	{
+		public User Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<User> Select()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int Insert(User record)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(User record)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
