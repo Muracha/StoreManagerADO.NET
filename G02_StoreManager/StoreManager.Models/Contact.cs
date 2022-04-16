70f38b15c0c010ac4151dd8418c6634aa91d7a00
XsoftScript <103327624+XsoftScript@users.noreply.github.com>
2022-04-16 14:35:22 +0400

    Change Contact

--- G02_StoreManager/StoreManager.Models/Contact.cs
+++ G02_StoreManager/StoreManager.Models/Contact.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace StoreManager.Models
 {
-    class Contact
+    public class Contact
     {
         public int ID { get; set; }
         public string Type { get; set; }
