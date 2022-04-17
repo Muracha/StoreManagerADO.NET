de2888578f7b0782326c64372755e3a02539bc9b
XsoftScript <103327624+XsoftScript@users.noreply.github.com>
2022-04-17 22:13:09 +0400

    add ContactInfoRepository and adeed ContactInfoRepository methods

---
+++ G02_StoreManager/StoreManager.Models/ContactInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Models
+{
+    public class ContactInfo
+    {
+        public int ID { get; set; }
+        public int EmployeeID { get; set; }
+        public int ContactType { get; set; }
+        public string ContactData { get; set; }
+        public bool IsPrimary { get; set; }
+        public string CreateDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ G02_StoreManager/StoreManager.Repositories/ContactInfoRepository.cs
@@ -0,0 +1,98 @@
+﻿using DataHelper;
+using StoreManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StoreManager.Repositories
+{
+    public class ContactInfoRepository
+    {
+        public void Insert(ContactInfo record)
+        {
+            using (Database _database = new Database(@"server = DESKTOP-GGJH7CA; database = G02_Store; integrated security = true", true))
+            {
+                try
+                {
+                    _database.BeginTransaction();
+                    _database.ExecuteNonQuery("InsertContactInfo_SP", CommandType.StoredProcedure,
+                        new SqlParameter("@EmployeeID", record.EmployeeID),
+                        new SqlParameter("@ContactType", record.ContactType),
+                        new SqlParameter("@ContactData", record.ContactData),
+                        new SqlParameter("@IsPrimary", record.IsPrimary));
+
+                    _database.CommitTransaction();
+                }
+                catch (Exception ex)
+                {
+                    _database.RollBack();
+                    throw ex;
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (Database _database = new Database(@"server = DESKTOP-GGJH7CA; database = G02_Store; integrated security = true", true))
+            {
+                try
+                {
+                    _database.BeginTransaction();
+                    _database.ExecuteNonQuery("InsertContactInfo_SP", CommandType.StoredProcedure, new SqlParameter("@ID", id));
+  
+                    _database.CommitTransaction();
+                }
+                catch (Exception ex)
+                {
+                    _database.RollBack();
+                    throw ex;
+                }
+            }
+        }
+
+        public IEnumerable<ContactInfo> Select()
+        {
+            using (Database _database = new Database(@"server = DESKTOP-GGJH7CA; database = G02_Store; integrated security = true"))
+            {
+                var reader = _database.ExecuteReader("SelectContactInfo_SP", CommandType.StoredProcedure);
+                foreach (IDataRecord record in reader)
+                {
+                    ContactInfo contactInfo = new ContactInfo();
+                    contactInfo.ID = record.GetInt32(0);
+                    contactInfo.EmployeeID = record.GetInt32(1);
+                    contactInfo.ContactType = record.GetInt32(2);
+                    contactInfo.ContactData = record.GetDateTime(3).ToString("dd/MM/yyyy");
+                    contactInfo.IsPrimary = record.GetBoolean(4);
+                    contactInfo.CreateDate = record.GetDateTime(5).ToString("dd/MM/yyyy");
+                    contactInfo.IsDeleted = record.GetBoolean(6);
+                    yield return contactInfo;
+                }
+            }
+        }
+
+        public void Update(ContactInfo record)
+        {
+            using (Database _database = new Database(@"server = DESKTOP-GGJH7CA; database = G02_Store; integrated security = true", true))
+            {
+                try
+                {
+                    _database.BeginTransaction();
+                    _database.ExecuteNonQuery("UpdateContactInfo_SP", CommandType.StoredProcedure,
+                                           new SqlParameter("@ID", record.ID),
+                                           new SqlParameter("@EmployeeID ", record.EmployeeID),
+                                           new SqlParameter("@ContactType ", record.ContactType),
+                                           new SqlParameter("@ContactData", record.ContactData),
+                                           new SqlParameter("@IsPrimary", record.IsPrimary)
+                                           );
+                    _database.CommitTransaction();
+                }
+                catch (Exception ex)
+                {
+                    _database.RollBack();
+                    throw ex;
+                }
+            }
+        }
+    }
+}
