574b0c2b5a9089da8e1fa8d542245e0a4507f5c0
David Dvali <david.dvali@gmail.com>
2022-04-19 19:57:12 +0400

    Add RepositoryBase blank class

---
+++ G02_StoreManager/StoreManager.Repositories/RepositoryBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Repositories
+{
+	public abstract class RepositoryBase<T>
+	{
+		public virtual T Get(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual IEnumerable<T> Select()
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual int Insert(T record)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void Update(T record)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void Delete(int id)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
