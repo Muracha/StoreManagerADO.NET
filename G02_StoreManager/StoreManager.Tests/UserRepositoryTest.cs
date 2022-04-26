be3df2e6c86b99369497b2c30d1541d1a0c111dc
David Dvali <david.dvali@gmail.com>
2022-04-26 20:02:30 +0400

    UnitTest template for UserRepository

---
+++ G02_StoreManager/StoreManager.Tests/UserRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreManager.Models;
+using StoreManager.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.Tests
+{
+
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        private UserRepository _userRepository;
+
+        public UserRepositoryTest()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        [TestMethod]
+        public void A_TestInsert()
+        {
+            var user = new User() { };
+            int id = _userRepository.Insert(user);
+            Assert.IsTrue(id > 0);
+        }
+
+        [TestMethod]
+        public void B_TestUpdate()
+        {
+
+        }
+
+        [TestMethod]
+        public void C_TestDelete()
+        {
+
+        }
+    }
+}
