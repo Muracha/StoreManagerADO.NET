48d66a899d495e2d2b8c75512929ad375334e2df
XsoftScript <103327624+XsoftScript@users.noreply.github.com>
2022-07-02 13:14:19 +0400

    Added to EmployeeRepository(): GetEmployeesNotRegisteredUser() and Added  EmployeeService() : return GetEmployeesNotRegisteredUser()

--- G02_StoreManager/StoreManager.Repositories/EmployeeRepository.cs
+++ G02_StoreManager/StoreManager.Repositories/EmployeeRepository.cs
@@ -11,6 +11,16 @@ namespace StoreManager.Repositories
 {
     public class EmployeeRepository : RepositoryBase<Employee>
     {
-
+        public IEnumerable<Employee> GetEmployeesNotRegisteredUser()
+        {
+            var table = _database.GetTable($"GetEmployeesToAdded_SP", CommandType.StoredProcedure);
+            if (table.Rows.Count > 0)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    yield return GetItem(row);
+                }
+            }
+        }
     }
 }
--- G02_StoreManager/StoreManager.Services/EmployeeService.cs
+++ G02_StoreManager/StoreManager.Services/EmployeeService.cs
@@ -1,9 +1,14 @@
 ﻿using StoreManager.Models;
 using StoreManager.Repositories;
+using System.Collections.Generic;
 
 namespace StoreManager.Services
 {
     public class EmployeeService : ServiceRepositoryBase<Employee, EmployeeRepository>
     {
+        public IEnumerable<Employee> GetEmployeesNotRegisteredUser()
+        {  
+            return _repository.GetEmployeesNotRegisteredUser();
+        }
     }
 }
